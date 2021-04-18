567ced67e5f59fe2f9e8f230939a48bfcc871f9e
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2021-04-18 23:18:24 +0300

    added inputs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 10.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwdardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwdardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwdardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
